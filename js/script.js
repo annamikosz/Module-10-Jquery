5eddd4db23689490a24778cc4218dd735068c3a4
Anna Mikosz <annamikosz.lu@gmail.com>
2017-06-17 17:30:27 +0200

    JQuery Carousel

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+  var carouselList = $("#carousel ul");
+
+  
+  function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+  }
+  function moveFirstSlide(){
+    var firstItem = carouselList.find("li:first"),
+      lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({'marginLeft':0});
+
+}
+
+  setInterval(changeSlide, 3000);
+});
