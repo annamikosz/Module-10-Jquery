c0db29703f876e21db5cbb9e2a24a5c364b7efb9
Anna Mikosz <annamikosz.lu@gmail.com>
2017-06-20 16:51:32 +0200

    JQuery carousel with buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
 $(function() {
   var carouselList = $("#carousel ul");
-
+  var rightArrow = $('.right-arrow').find('i'),
+    leftArrow = $('.left-arrow').find('i');
   
+ 
+  
+  function changeSlideLeft() {
+    carouselList.animate({'marginLeft': 0}, 500, moveFirstSlideLeft);
+  }
+  function moveFirstSlideLeft(){
+    var firstItem = carouselList.find("li:first"),
+      lastItem = carouselList.find("li:last");
+    firstItem.before(lastItem);
+
+      carouselList.css({'marginLeft': -400});
+  }
+    function changeSlideRight() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlideRight);
+  }
+  function moveFirstSlideRight(){
+    var firstItem = carouselList.find("li:first"),
+      lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    
+    carouselList.css({'marginLeft':0});
+
+}
   function changeSlide() {
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    carouselList.animate({'marginLeft':0}, 500, moveFirstSlide);
   }
   function moveFirstSlide(){
     var firstItem = carouselList.find("li:first"),
       lastItem = carouselList.find("li:last");
     lastItem.after(firstItem);
+    
     carouselList.css({'marginLeft':0});
 
 }
-
-  setInterval(changeSlide, 3000);
+ 
+setInterval(changeSlide, 5000);
+  
+rightArrow.on('click', function() {
+    changeSlideRight();
+});
+leftArrow.on('click', function() {
+    changeSlideLeft();
+});
+     
 });
